750be06163a246ceac0913e5b0eb6f4d5fdbcd1f
Dmitry Ponomarev <dponomarev@luxoft.com>
2018-04-04 19:47:01 +0200

    put ./ in every path

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@
 
 
 
-rem java  -jar ./target/happycube-spring-boot.jar ./data blue txt ./solutions/blue solution sol false true
+java  -jar ./target/happycube-spring-boot.jar ./data blue txt ./solutions/blue solution sol false true
 
-java  -jar ./target/happycube-spring-boot.jar ./data red txt ./solutions/red solution sol true true
+rem java  -jar ./target/happycube-spring-boot.jar ./data red txt ./solutions/red solution sol true true
 
 rem java  -jar ./target/happycube-spring-boot.jar ./data lilac txt ./solutions/lilac_new1 solution sol false true
 
